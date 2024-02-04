40d1deff045b533364e546f1155f6d5410039390
Rifat Khan <rifat6633@gmail.com>
2024-02-04 21:06:10 +0600

    refactor: loop effects to render dynamic card

--- app/(main)/effects/_parts/effects-styles.tsx
+++ app/(main)/effects/_parts/effects-styles.tsx
@@ -1,15 +1,25 @@
+import { effects } from "@/components/effects/effects";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function EffectsAndStylesList() {
   return (
     <section className="grid grid-cols-4 gap-2">
-      <Card>
-        <div className="w-full h-40 bg-fuchsia-200"></div>
+      {effects.map((effect) => (
+        <Card key={effect.herf}>
+          <Link href={effect.herf}>
+            <div className="w-full pb-[100%]"></div>
+          </Link>
 
-        <CardHeader>
-          <CardTitle className="text-center">Hello</CardTitle>
-        </CardHeader>
-      </Card>
+          <CardHeader>
+            <CardTitle className="text-center">
+              <Link href={effect.herf} className="hover:underline">
+                {effect.title}
+              </Link>
+            </CardTitle>
+          </CardHeader>
+        </Card>
+      ))}
     </section>
   );
 }
