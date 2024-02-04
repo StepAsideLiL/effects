7edc66c477b9a815806b0c8db99bcd59f5097dae
Rifat Khan <rifat6633@gmail.com>
2024-02-04 15:57:30 +0600

    refactor: width and height of the logo

--- components/uis/logo.tsx
+++ components/uis/logo.tsx
@@ -18,20 +18,20 @@ export default function Logo({
 
 function Icon() {
   return (
-    <div className="w-14 h-14 flex justify-between items-center gap-1 rotate-45">
+    <div className="w-10 h-10 flex justify-between items-center gap-1 rotate-45">
       <div className="flex flex-col gap-2 items-end">
-        <div className="bg-foreground w-2 h-1 rounded"></div>
-        <div className="bg-foreground w-5 h-1 rounded"></div>
-        <div className="bg-foreground w-2 h-1 rounded"></div>
+        <div className="bg-foreground w-1.5 h-0.5 rounded"></div>
+        <div className="bg-foreground w-3.5 h-0.5 rounded"></div>
+        <div className="bg-foreground w-1.5 h-0.5 rounded"></div>
       </div>
 
-      <div className="bg-foreground w-1 h-full rounded"></div>
+      <div className="bg-foreground w-0.5 h-full rounded"></div>
 
       <div>
         <div className="flex flex-col gap-2 items-start">
-          <div className="bg-foreground w-2 h-1 rounded"></div>
-          <div className="bg-foreground w-5 h-1 rounded"></div>
-          <div className="bg-foreground w-2 h-1 rounded"></div>
+          <div className="bg-foreground w-1.5 h-0.5 rounded"></div>
+          <div className="bg-foreground w-3.5 h-0.5 rounded"></div>
+          <div className="bg-foreground w-1.5 h-0.5 rounded"></div>
         </div>
       </div>
     </div>
