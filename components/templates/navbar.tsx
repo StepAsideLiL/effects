ecb2ab7f2b17dbaa6041fed3b41bc7c23c5d97c0
Rifat Khan <rifat6633@gmail.com>
2024-02-04 15:49:33 +0600

    add: logo to the navbar

--- components/templates/navbar.tsx
+++ components/templates/navbar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { ModeToggle } from "./mode-toggle";
+import Logo from "@/components/uis/logo";
 
 export default function Navbar() {
   return (
-    <header>
-      <div></div>
+    <header className="container py-4 flex justify-between items-center">
+      <div>
+        <Logo variant="link" />
+      </div>
 
       <div>
         <ModeToggle />
