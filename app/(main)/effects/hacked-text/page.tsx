a32eae3a480358a895a3775856c77e4ebb3efd9a
Rifat Khan <rifat6633@gmail.com>
2024-02-04 20:59:30 +0600

    create: hacked text effect perview and doc page

---
+++ app/(main)/effects/hacked-text/page.tsx
@@ -0,0 +1,10 @@
+import HackedText from "@/components/effects/hacked-text";
+import { Main } from "@/components/uis/main";
+
+export default function HackedTextPage() {
+  return (
+    <Main variant={"center"}>
+      <HackedText className="text-4xl">Hacked Text</HackedText>
+    </Main>
+  );
+}
