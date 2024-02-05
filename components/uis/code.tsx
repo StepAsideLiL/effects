523b9f032c916e55ab00df288f007f03c48c65ab
Rifat Khan <rifat6633@gmail.com>
2024-02-05 10:59:27 +0600

    update: Code component props

--- components/uis/code.tsx
+++ components/uis/code.tsx
@@ -1,36 +1,67 @@
 "use client";
 
 import { CodeBlock, CopyBlock, tomorrowNight } from "react-code-blocks";
+import { Separator } from "@/components/ui/separator";
 
 export default function Code({
   code,
+  filename = "",
+  language = "jsx",
   copy = true,
   lineNumber = true,
 }: {
   code: string;
+  filename?: string;
+  language?: string;
   copy?: boolean;
   lineNumber?: boolean;
 }) {
   if (copy) {
     return (
-      <CopyBlock
+      <>
+        {filename && (
+          <>
+            <CodeBlock
+              text={"hacked-text.tsx"}
+              language={language}
+              showLineNumbers={false}
+              theme={tomorrowNight}
+            />
+            <Separator orientation="horizontal" />
+          </>
+        )}
+        <CopyBlock
+          text={code}
+          language={language}
+          showLineNumbers={lineNumber}
+          startingLineNumber={1}
+          theme={tomorrowNight}
+          codeBlock
+        />
+      </>
+    );
+  }
+
+  return (
+    <>
+      {filename && (
+        <>
+          <CodeBlock
+            text={"hacked-text.tsx"}
+            language={"bash"}
+            showLineNumbers={false}
+            theme={tomorrowNight}
+          />
+          <Separator orientation="horizontal" />
+        </>
+      )}
+      <CodeBlock
         text={code}
         language={"jsx"}
         showLineNumbers={lineNumber}
         startingLineNumber={1}
         theme={tomorrowNight}
-        codeBlock
       />
-    );
-  }
-
-  return (
-    <CodeBlock
-      text={code}
-      language={"jsx"}
-      showLineNumbers={lineNumber}
-      startingLineNumber={1}
-      theme={tomorrowNight}
-    />
+    </>
   );
 }
