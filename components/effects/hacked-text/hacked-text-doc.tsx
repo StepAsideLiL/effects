9564a0125d41e738d96e2580c0843cd5a8735aa1
Rifat Khan <rifat6633@gmail.com>
2024-02-05 15:17:54 +0600

    add: info and style

--- components/effects/hacked-text/hacked-text-doc.tsx
+++ components/effects/hacked-text/hacked-text-doc.tsx
@@ -1,5 +1,13 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
 import Code from "@/components/uis/code";
+import {
+  DocContain,
+  DocDecscription,
+  DocLink,
+  DocSection,
+  DocSectionTitle,
+  DocTitle,
+} from "@/components/uis/docs-ui";
 import { promises as fs } from "fs";
 
 export default async function HackedTextDoc() {
@@ -9,11 +17,35 @@ export default async function HackedTextDoc() {
   );
 
   return (
-    <section className="container max-w-3xl mx-auto py-10">
-      <h3 className="text-2xl font-semibold">Code</h3>
-      <ScrollArea className="h-80">
-        <Code code={file} filename="hacked-text.tsx" />
-      </ScrollArea>
-    </section>
+    <DocContain>
+      <DocSection>
+        <DocTitle>Hacked Text</DocTitle>
+        <DocDecscription>
+          On mouseover, the text is randomized, and it returns to the initial
+          text one letter at a time.
+        </DocDecscription>
+      </DocSection>
+
+      <DocSection>
+        <DocSectionTitle>Required</DocSectionTitle>
+        <DocLink href="https://ui.shadcn.com/docs/installation/next">
+          Nextjs and Shadcn/ui
+        </DocLink>
+      </DocSection>
+
+      <DocSection>
+        <DocSectionTitle>Code</DocSectionTitle>
+        <ScrollArea className="h-80">
+          <Code code={file} filename="hacked-text.tsx" />
+        </ScrollArea>
+      </DocSection>
+
+      <DocSection>
+        <DocSectionTitle>Inspired From</DocSectionTitle>
+        <DocLink href="https://youtu.be/W5oawMJaXbU?si=QrGQQkp-_mVBtn9a">
+          Hyperplexed Video
+        </DocLink>
+      </DocSection>
+    </DocContain>
   );
 }
