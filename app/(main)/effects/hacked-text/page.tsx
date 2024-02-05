5c97a8ee268937690c7544f923ac0de1a8e69817
Rifat Khan <rifat6633@gmail.com>
2024-02-05 15:16:28 +0600

    update: with custom component

--- app/(main)/effects/hacked-text/page.tsx
+++ app/(main)/effects/hacked-text/page.tsx
@@ -1,13 +1,16 @@
 import HackedTextDoc from "@/components/effects/hacked-text/hacked-text-doc";
 import HackedText from "@/components/effects/hacked-text/hacked-text";
 import { Separator } from "@/components/ui/separator";
+import { ComponentContain, MoveToDocLink } from "@/components/uis/docs-ui";
 
 export default function HackedTextPage() {
   return (
     <main>
-      <section className="min-h-screen flex items-center justify-center">
-        <HackedText className="text-4xl">Hacked Text</HackedText>
-      </section>
+      <ComponentContain>
+        <HackedText className="text-9xl">Hacked Text</HackedText>
+
+        <MoveToDocLink />
+      </ComponentContain>
 
       <Separator orientation="horizontal" />
 
