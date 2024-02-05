07d31e7280f2f10a951a2667946c2aa3bdb88b4f
Rifat Khan <rifat6633@gmail.com>
2024-02-05 12:27:30 +0600

    add: HackedTextDoc component doc

--- app/(main)/effects/hacked-text/page.tsx
+++ app/(main)/effects/hacked-text/page.tsx
@@ -1,10 +1,17 @@
-import HackedText from "@/components/effects/effects-ui/hacked-text";
-import { Main } from "@/components/uis/main";
+import HackedTextDoc from "@/components/effects/hacked-text/hacked-text-doc";
+import HackedText from "@/components/effects/hacked-text/hacked-text";
+import { Separator } from "@/components/ui/separator";
 
 export default function HackedTextPage() {
   return (
-    <Main variant={"center"}>
-      <HackedText className="text-4xl">Hacked Text</HackedText>
-    </Main>
+    <main>
+      <section className="min-h-screen flex items-center justify-center">
+        <HackedText className="text-4xl">Hacked Text</HackedText>
+      </section>
+
+      <Separator orientation="horizontal" />
+
+      <HackedTextDoc />
+    </main>
   );
 }
