8741fbd445879bce4211e252b5e102f23109f278
Rifat Khan <rifat6633@gmail.com>
2024-02-05 17:45:36 +0600

    create: banner design

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -1,4 +1,10 @@
+import HackedText from "@/components/effects/hacked-text/hacked-text";
+import { Main } from "@/components/uis/main";
+import { NextjsIcon, ShadcnIcon } from "@/components/uis/svg-icons";
+import { rubikMonoOne } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -6,6 +12,31 @@ export const metadata: Metadata = {
 
 export default function HomePage() {
   return (
-    <main className="justify-center text-center py-10">Hello, Effects</main>
+    <Main>
+      <section className="space-y-10 py-10">
+        <h3 className="text-xl text-muted-foreground text-center">
+          Copy and paste component on your project
+        </h3>
+        <HackedText
+          className={cn(
+            rubikMonoOne.className,
+            "w-fit mx-auto text-center text-6xl"
+          )}
+        >
+          EFFECTS & STYLES
+        </HackedText>
+
+        <p className="text-xl font-medium text-center">FOR</p>
+
+        <section className="flex justify-center items-center gap-10">
+          <Link href={`https://nextjs.org/`}>
+            <NextjsIcon />
+          </Link>
+          <Link href={`https://ui.shadcn.com/`}>
+            <ShadcnIcon />
+          </Link>
+        </section>
+      </section>
+    </Main>
   );
 }
