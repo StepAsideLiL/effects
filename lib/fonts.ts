0d8dc49d62815576fc68b8ed3d0a8cdd659f7af7
Rifat Khan <rifat6633@gmail.com>
2024-02-05 15:37:16 +0600

    add: Rubik Mono One

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,8 +1,13 @@
-import { Inter, Poppins } from "next/font/google";
+import { Inter, Poppins, Rubik_Mono_One } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 
 export const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const rubikMonoOne = Rubik_Mono_One({
+  subsets: ["latin"],
+  weight: ["400"],
 });
