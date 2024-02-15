2cd5ff45d7c9a77f08e80728769eee817eca632e
Rifat Khan <rifat6633@gmail.com>
2024-02-15 22:58:49 +0600

    add: Animated Underline menu

--- components/effects/effects.ts
+++ components/effects/effects.ts
@@ -1,5 +1,11 @@
 export const effects = [
   {
+    title: "Animated Underline",
+    herf: "/effects/animated-underline",
+    imgSrc: "",
+    type: "effect",
+  },
+  {
     title: "Hacked Text",
     herf: "/effects/hacked-text",
     imgSrc: "",
