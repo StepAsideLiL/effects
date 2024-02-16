026d6f0aceb7f686b79a3d483d191f20a3452436
Rifat Khan <rifat6633@gmail.com>
2024-02-16 18:13:11 +0600

    create: CopyText component

---
+++ components/effects/copy-text/copy-text.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Copy, CopyCheck } from "lucide-react";
+import { useState } from "react";
+
+export default function CopyText({
+  children,
+  className,
+}: {
+  children: string;
+  className?: string;
+}) {
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(children);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 1500);
+    } catch (error) {
+      console.error("Failed to copy:", error);
+    }
+  };
+
+  return (
+    <div
+      className={cn(
+        "font-mono flex justify-between items-center w-[512px] p-5 border rounded",
+        className
+      )}
+    >
+      <p>{children}</p>
+
+      <button onClick={() => copyToClipboard()}>
+        {copied ? <CopyCheck size={"16"} /> : <Copy size={"16"} />}
+      </button>
+    </div>
+  );
+}
