bdf08d05c49aa2818fd5c302242e8b480468fb29
Rifat Khan <rifat6633@gmail.com>
2024-02-16 15:45:42 +0600

    create: code-block page

---
+++ app/(main)/effects/code-block/page.tsx
@@ -0,0 +1,26 @@
+import { Separator } from "@/components/ui/separator";
+import { ComponentContain, MoveToDocLink } from "@/components/uis/docs-ui";
+import { promises as fs } from "fs";
+import CodeBlock from "@/components/effects/code-block/code-block";
+import CodeBlockDoc from "@/components/effects/code-block/code-block-doc";
+
+export default async function Page() {
+  const file = await fs.readFile(
+    process.cwd() + "/components/effects/code-block/code-block.tsx",
+    "utf8"
+  );
+
+  return (
+    <main>
+      <ComponentContain>
+        <CodeBlock code={file} filename="code-block.tsx" hfit={false} />
+
+        <MoveToDocLink />
+      </ComponentContain>
+
+      <Separator orientation="horizontal" />
+
+      <CodeBlockDoc />
+    </main>
+  );
+}
