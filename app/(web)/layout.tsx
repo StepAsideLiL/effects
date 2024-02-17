425e5b3b553576987017c2d6509fc4c3085e2d79
Rifat Khan <rifat6633@gmail.com>
2024-02-17 10:35:37 +0600

    update: remove imports

--- app/(web)/layout.tsx
+++ app/(web)/layout.tsx
@@ -1,4 +1,6 @@
+import EffectsSidebar from "@/components/templates/effects-sidebar";
 import Navbar from "@/components/templates/navbar";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function WebLayout({ children }: { children: React.ReactNode }) {
   return (
