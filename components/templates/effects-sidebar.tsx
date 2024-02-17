295321f87a6ff8782fb871d323b92c1d75b79a58
Rifat Khan <rifat6633@gmail.com>
2024-02-17 11:03:50 +0600

    update: add nav menus

--- components/templates/effects-sidebar.tsx
+++ components/templates/effects-sidebar.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import { effects } from "../effects/effects";
 import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+import NavMenu from "./nav-menu";
 
 export default function EffectsSidebar() {
   return (
     <nav className="flex flex-col gap-2">
+      <NavMenu orientation="vertical" />
+      <Separator />
       {effects.map((list) => (
         <Link
           key={list.herf}
