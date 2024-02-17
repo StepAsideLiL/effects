371fca1ab4ae926a04a8521d1085ab26eb0bb9e5
Rifat Khan <rifat6633@gmail.com>
2024-02-17 11:18:25 +0600

    add: effects menu title and update text style

--- components/templates/effects-menu-sheet.tsx
+++ components/templates/effects-menu-sheet.tsx
@@ -39,13 +39,14 @@ export default function EffectsMenuSheet() {
           <nav className="flex flex-col gap-2">
             <NavMenu orientation="vertical" />
             <Separator />
+            <h1 className="text-lg font-semibold">Effects & Styles</h1>
             {effects.map((list) => (
               <Link
                 key={list.herf}
                 href={list.herf}
                 className={cn(
-                  "text-sm hover:underline",
-                  pathname === list.herf && "underline"
+                  "text-muted-foreground text-sm hover:underline",
+                  pathname === list.herf && "text-foreground underline"
                 )}
                 onClick={() => setOpen(false)}
               >
--- components/templates/effects-sidebar.tsx
+++ components/templates/effects-sidebar.tsx
@@ -9,11 +9,12 @@ export default function EffectsSidebar() {
     <nav className="flex flex-col gap-2">
       <NavMenu orientation="vertical" />
       <Separator />
+      <h1 className="text-lg font-semibold">Effects & Styles</h1>
       {effects.map((list) => (
         <Link
           key={list.herf}
           href={list.herf}
-          className={cn("text-sm hover:underline")}
+          className={cn("text-muted-foreground text-sm hover:underline")}
         >
           {list.title}
         </Link>
