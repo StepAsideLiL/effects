c29a10ae61eb88993af6d9d5bfa709250d8be0aa
Rifat Khan <rifat6633@gmail.com>
2024-02-17 10:34:54 +0600

    update: responsive navbar

--- components/templates/navbar.tsx
+++ components/templates/navbar.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { ModeToggle } from "./mode-toggle";
 import Logo from "@/components/uis/logo";
 import NavMenu from "./nav-menu";
+import EffectsMenuSheet from "./effects-menu-sheet";
 
 export default function Navbar() {
   return (
     <header className="container py-4 flex justify-between items-center">
-      <div className="flex items-center gap-5">
+      <div className="lg:flex hidden items-center gap-5">
         <Logo variant="link" />
 
         <NavMenu />
+      </div>
+
+      <div className="lg:hidden block">
+        <EffectsMenuSheet />
       </div>
 
       <div>
