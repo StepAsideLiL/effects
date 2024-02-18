c8735984adcd2f7e0023db9941315174d26d7271
Rifat Khan <rifat6633@gmail.com>
2024-02-18 21:46:03 +0600

    create: file consistant of all menus

---
+++ lib/menus.ts
@@ -0,0 +1,46 @@
+export const menus = {
+  mainHeaderMenu: [
+    {
+      title: "Docs",
+      href: "/docs",
+    },
+    {
+      title: "Effects",
+      href: "/effects",
+    },
+    {
+      title: "Front pages",
+      href: "/front-pages",
+    },
+    {
+      title: "Designs",
+      href: "/designs",
+    },
+  ],
+  effectsList: [
+    {
+      title: "Animated Underline",
+      href: "/effects/animated-underline",
+      type: "effect",
+      tags: ["effect", "text", "underline"],
+    },
+    {
+      title: "Code Block",
+      href: "/effects/code-block",
+      type: "style",
+      tags: ["style", "code"],
+    },
+    {
+      title: "Copy Text",
+      href: "/effects/copy-text",
+      type: "style",
+      tags: ["style", "text", "copy"],
+    },
+    {
+      title: "Hacked Text",
+      href: "/effects/hacked-text",
+      type: "effect",
+      tags: ["effect", "text"],
+    },
+  ],
+};
