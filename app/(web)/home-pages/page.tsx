9c882fe103a5967077d1abb735c948973bfad83e
Rifat Khan <rifat6633@gmail.com>
2024-02-19 11:29:42 +0600

    create: Home Pages page

---
+++ app/(web)/home-pages/page.tsx
@@ -0,0 +1,24 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen container space-y-10">
+      <h1 className="py-10 text-center text-2xl font-semibold">
+        Every Website Deserve A Beautiful Website
+      </h1>
+
+      <section className="grid grid-cols-3 gap-2">
+        <Card>
+          <CardHeader>
+            <CardTitle>
+              <Link href={`/home-pages/01`} className="hover:underline">
+                01
+              </Link>
+            </CardTitle>
+          </CardHeader>
+        </Card>
+      </section>
+    </main>
+  );
+}
