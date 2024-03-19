8caa8c3d4e67ab05ad04052d155a5acbf3feea49
Rifat Khan <rifat6633@gmail.com>
2024-03-19 12:15:58 +0600

    update: import effects menu from menus

--- app/(effects-list)/effects/_parts/effects-styles.tsx
+++ app/(effects-list)/effects/_parts/effects-styles.tsx
@@ -1,12 +1,12 @@
 import { AnimatedUnderline } from "@/components/effects/animated-underline/animated-underline";
-import { effects } from "@/components/effects/effects";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { menus } from "@/lib/menus";
 import Link from "next/link";
 
 export default function EffectsAndStylesList() {
   return (
     <section className="grid grid-cols-4 gap-2">
-      {effects.map((effect) => (
+      {menus.effectsList.map((effect) => (
         <Card key={effect.href}>
           <CardHeader>
             <CardTitle className="text-center text-lg">
