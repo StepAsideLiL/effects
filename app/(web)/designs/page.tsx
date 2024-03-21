fdf638bf1db9d307b648c37993cb1d8b26a2fd39
Rifat Khan <rifat6633@gmail.com>
2024-03-21 15:44:04 +0600

    create: designs page

---
+++ app/(web)/designs/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <main className="container min-h-screen space-y-4 py-4">
+      <h1 className="text-center text-4xl text-muted">Cooming Soon</h1>
+    </main>
+  );
+}
