ae75c7b9d85fe825d2ec197b3a34564f681b4971
Rifat Khan <rifat6633@gmail.com>
2024-03-24 16:55:39 +0600

    remove: EffectsNavbar and place header directly to effect-preview-doc group layout

--- app/(effect-preview-doc)/layout.tsx
+++ app/(effect-preview-doc)/layout.tsx
@@ -1,4 +1,8 @@
-import EffectsNavbar from "@/components/templates/effects-navbar";
+import EffectsMenuSheet from "@/components/templates/effects-menu-sheet";
+import SearchCommandMenu from "@/components/templates/search-command-menu";
+import { Button } from "@/components/ui/button";
+import { MoveLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function MainLayout({
   children,
@@ -7,7 +11,18 @@ export default function MainLayout({
 }) {
   return (
     <>
-      <EffectsNavbar />
+      <header className="container fixed top-3 z-50 flex items-center gap-3">
+        <EffectsMenuSheet />
+
+        <Button variant={"ghost"} size={"icon"}>
+          <Link href={"/effects"}>
+            <span className="sr-only">Back to effects page</span>
+            <MoveLeft />
+          </Link>
+        </Button>
+
+        <SearchCommandMenu />
+      </header>
 
       {children}
     </>
