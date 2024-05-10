b6e6fb385aadcebb6d7ed0725273f24c3afd9da0
Rifat Khan <rifat6633@gmail.com>
2024-05-10 21:15:52 +0600

    create: total section and container section tags

--- app/(home-page-design)/home-pages/02/page.tsx
+++ app/(home-page-design)/home-pages/02/page.tsx
@@ -1,3 +1,29 @@
 export default function Page() {
-  return <div>/home-pages/02</div>;
+  return (
+    <main className="container">
+      {/* Banner */}
+      <section></section>
+
+      {/* Products */}
+      <section></section>
+
+      {/* Features */}
+      <section></section>
+
+      {/* Choose us */}
+      <section></section>
+
+      {/* Visions */}
+      <section></section>
+
+      {/* Physics elements */}
+      <section></section>
+
+      {/* Trusted by */}
+      <section></section>
+
+      {/* Contact */}
+      <section></section>
+    </main>
+  );
 }
