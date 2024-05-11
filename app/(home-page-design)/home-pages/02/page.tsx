f7991ff7a0a1a58fcc535f72ac7d642933211b30
Rifat Khan <rifat6633@gmail.com>
2024-05-11 19:02:58 +0600

    design: vision section

--- app/(home-page-design)/home-pages/02/page.tsx
+++ app/(home-page-design)/home-pages/02/page.tsx
@@ -24,6 +24,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
 
 const features = [
   {
@@ -73,6 +75,30 @@ const whyChooseUs = [
     title: "New and improved",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sapiente saepe nulla amet sunt odio doloremque, distinctio optio tempore. Voluptatum recusandae repudiandae quia iure voluptas, reiciendis modi quidem nam itaque.",
+  },
+];
+
+const ourVisions = [
+  {
+    title: "Bringing creativity and innovation to your brand",
+    bagde: "Our Mission",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sapiente saepe nulla amet sunt odio doloremque, distinctio optio tempore. Voluptatum recusandae repudiandae quia iure voluptas, reiciendis modi quidem nam itaque.",
+    image: "/images/creativity-01.jpg",
+  },
+  {
+    title: "Growing ideas into reality",
+    bagde: "Our Vision",
+    description:
+      "Lorem ipsum remque, distinctio optio tempore. Voluptatum recusandae repudiandae quia iure voluptas, reiciendis modi quidem nam dolor sit amet consectetur adipisicing elit. Quis sapiente saepe nulla amet sunt odio doloremque, distinctio optio tempore. Voluptatum recusandae repudiandae quia iure voluptas, reiciendis modi quidem nam itaque.",
+    image: "/images/banner-1.jpg",
+  },
+  {
+    title: "Providing you with the greatest tech team",
+    bagde: "Our Commitment",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sapiente saepe nulla amet sunt odio doloremque, distinctio optio tempore. Voluptatum recusandae repudiandae quia iure voluptas, reiciendis modi quidem nam itaque.",
+    image: "/images/team-01.jpg",
   },
 ];
 
@@ -235,7 +261,32 @@ export default function Page() {
       </section>
 
       {/* Visions */}
-      <section></section>
+      <section className="py-20">
+        <div className="min-h-screen w-full">
+          {ourVisions.map((list, i) => (
+            <div key={i} className="flex w-full items-center">
+              <div className="w-full">
+                <Image src={list.image} alt="Image" width={1000} height={100} />
+              </div>
+
+              <div className="flex w-full flex-col">
+                <div className="mx-auto max-w-lg">
+                  <Badge variant={"secondary"} className="w-fit">
+                    {list.bagde}
+                  </Badge>
+                  <Balancer
+                    className="mb-8 mt-2 text-4xl font-semibold"
+                    as={"h1"}
+                  >
+                    {list.title}
+                  </Balancer>
+                  <p>{list.description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
 
       {/* Physics elements */}
       <section></section>
