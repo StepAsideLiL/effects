e724fc73055c14a46356eeff45f5a850c8b8b5dd
Rifat Khan <rifat6633@gmail.com>
2024-05-11 21:25:13 +0600

    create: input and textarea component

---
+++ app/(home-page-design)/home-pages/02/_uis/textarea.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "flex min-h-32 w-full rounded-md border-b border-input bg-background py-3 text-sm ring-offset-background placeholder:text-primary focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Textarea.displayName = "Textarea";
+
+export { Textarea };
