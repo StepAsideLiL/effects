cbd28980515022d13680b5a69954466f622c0672
Rifat Khan <rifat6633@gmail.com>
2024-05-11 12:29:06 +0600

    create: animated-card component

---
+++ app/(home-page-design)/home-pages/02/_parts/animated-card.tsx
@@ -0,0 +1,86 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const AnimatedCard = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      className,
+    )}
+    {...props}
+  />
+));
+AnimatedCard.displayName = "AnimatedCard";
+
+const AnimatedCardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    {...props}
+  />
+));
+AnimatedCardHeader.displayName = "AnimatedCardHeader";
+
+const AnimatedCardTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h3
+    ref={ref}
+    className={cn(
+      "text-2xl font-semibold leading-none tracking-tight",
+      className,
+    )}
+    {...props}
+  />
+));
+AnimatedCardTitle.displayName = "AnimatedCardTitle";
+
+const AnimatedCardDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
+));
+AnimatedCardDescription.displayName = "AnimatedCardDescription";
+
+const AnimatedCardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+));
+AnimatedCardContent.displayName = "AnimatedCardContent";
+
+const AnimatedCardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex items-center p-6 pt-0", className)}
+    {...props}
+  />
+));
+AnimatedCardFooter.displayName = "AnimatedCardFooter";
+
+export {
+  AnimatedCard,
+  AnimatedCardHeader,
+  AnimatedCardFooter,
+  AnimatedCardTitle,
+  AnimatedCardDescription,
+  AnimatedCardContent,
+};
