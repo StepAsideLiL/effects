bbbbdac7d4ba34f91e6782968d9ee8a3db61d201
Rifat Khan <rifat6633@gmail.com>
2024-05-13 22:48:08 +0600

    create: seperate component for choose us section

--- app/(home-page-design)/home-pages/02/page.tsx
+++ app/(home-page-design)/home-pages/02/page.tsx
@@ -1,12 +1,5 @@
 import Balancer from "react-wrap-balancer";
 import { Separator } from "@/components/ui/separator";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import Image from "next/image";
 import { Badge } from "@/components/ui/badge";
 import { cn } from "@/lib/utils";
@@ -17,39 +10,7 @@ import Marquee from "react-fast-marquee";
 import Banner from "./_sections/banner";
 import Products from "./_sections/products";
 import Features from "./_sections/features";
-
-const whyChooseUs = [
-  {
-    title: "We are the Best",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sapiente saepe nulla amet sunt odio doloremque, distinctio optio tempore. Voluptatum recusandae repudiandae quia iure voluptas, reiciendis modi quidem nam itaque.",
-  },
-  {
-    title: "Best mobile app",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sapiente saepe nulla amet sunt odio doloremque, distinctio optio tempore. Voluptatum recusandae repudiandae quia iure voluptas, reiciendis modi quidem nam itaque.",
-  },
-  {
-    title: "Creative animation",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sapiente saepe nulla amet sunt odio doloremque, distinctio optio tempore. Voluptatum recusandae repudiandae quia iure voluptas, reiciendis modi quidem nam itaque.",
-  },
-  {
-    title: "Designed for you",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sapiente saepe nulla amet sunt odio doloremque, distinctio optio tempore. Voluptatum recusandae repudiandae quia iure voluptas, reiciendis modi quidem nam itaque.",
-  },
-  {
-    title: "Quick and easy",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sapiente saepe nulla amet sunt odio doloremque, distinctio optio tempore. Voluptatum recusandae repudiandae quia iure voluptas, reiciendis modi quidem nam itaque.",
-  },
-  {
-    title: "New and improved",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sapiente saepe nulla amet sunt odio doloremque, distinctio optio tempore. Voluptatum recusandae repudiandae quia iure voluptas, reiciendis modi quidem nam itaque.",
-  },
-];
+import ChooseUs from "./_sections/choose-us";
 
 const ourVisions = [
   {
@@ -156,31 +117,7 @@ export default function Page() {
       <Separator className="bg-primary" />
 
       {/* Choose us */}
-      <section className="mx-auto max-w-6xl space-y-24 py-20">
-        <div className="mx-auto max-w-lg">
-          <h1 className="text-center text-4xl font-medium">
-            Why Choose Our Service
-          </h1>
-        </div>
-
-        <div className="grid grid-cols-3 gap-10">
-          {whyChooseUs.map((list, i) => (
-            <Card key={i} className="w-full border-none bg-transparent">
-              <CardHeader className="relative">
-                <CardDescription className="font-medium text-muted-foreground">
-                  Step {i + 1}
-                </CardDescription>
-                <CardTitle className="w-2/3 text-4xl">{list.title}</CardTitle>
-                <span className="absolute right-3 top-3 -z-10 select-none text-[280px] font-bold leading-none text-muted">
-                  {i + 1}
-                </span>
-              </CardHeader>
-
-              <CardContent>{list.description}</CardContent>
-            </Card>
-          ))}
-        </div>
-      </section>
+      <ChooseUs />
 
       <Separator className="bg-primary" />
 
