388354a8eef907c76eb007eed38db57fcc973816
Rifat Khan <rifat6633@gmail.com>
2024-05-13 22:54:21 +0600

    create: seperate component for brands-and-agencies section

---
+++ app/(home-page-design)/home-pages/02/_sections/brands-and-agencies.tsx
@@ -0,0 +1,39 @@
+import Marquee from "react-fast-marquee";
+
+const companies = [
+  "SkyCode Inc.",
+  "Evergreen Tech Solutions",
+  "AstraNova Robotics",
+  "Tranquil Bay Studios",
+  "Zephyr Delivery Network",
+  "Luminary Labs",
+  "Mythos Design Collective",
+  "Nexus Bioengineering",
+  "Chrono Mechanics",
+  "Wanderlust Travel Co.",
+];
+
+export default function BrandsAndAgencies() {
+  return (
+    <section className="space-y-10 py-20">
+      <div className="mx-auto max-w-lg">
+        <h1 className="text-center text-2xl font-medium">
+          Top Brands & Agencies
+        </h1>
+      </div>
+
+      <div className="relative overflow-hidden before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-1/6 before:bg-gradient-to-r before:from-background before:to-foreground/0 before:content-[''] after:absolute after:right-0 after:top-0 after:z-10 after:h-full after:w-1/6 after:bg-gradient-to-r after:from-foreground/0 after:to-background after:content-['']">
+        <Marquee pauseOnClick autoFill>
+          {companies.map((company, i) => (
+            <div
+              key={i}
+              className="mx-20 flex size-56 select-none items-center justify-center overflow-hidden rounded-full bg-muted text-center text-2xl font-medium"
+            >
+              {company}
+            </div>
+          ))}
+        </Marquee>
+      </div>
+    </section>
+  );
+}
--- app/(home-page-design)/home-pages/02/page.tsx
+++ app/(home-page-design)/home-pages/02/page.tsx
@@ -1,29 +1,15 @@
 import Balancer from "react-wrap-balancer";
 import { Separator } from "@/components/ui/separator";
-import { cn } from "@/lib/utils";
 import { Input } from "./_uis/input";
 import { Textarea } from "./_uis/textarea";
 import { Button } from "@/components/ui/button";
-import Marquee from "react-fast-marquee";
 import Banner from "./_sections/banner";
 import Products from "./_sections/products";
 import Features from "./_sections/features";
 import ChooseUs from "./_sections/choose-us";
 import Visions from "./_sections/visions";
 import FallingElement from "./_sections/falling-element";
-
-const companies = [
-  "SkyCode Inc.",
-  "Evergreen Tech Solutions",
-  "AstraNova Robotics",
-  "Tranquil Bay Studios",
-  "Zephyr Delivery Network",
-  "Luminary Labs",
-  "Mythos Design Collective",
-  "Nexus Bioengineering",
-  "Chrono Mechanics",
-  "Wanderlust Travel Co.",
-];
+import BrandsAndAgencies from "./_sections/brands-and-agencies";
 
 export default function Page() {
   return (
@@ -53,26 +39,7 @@ export default function Page() {
       <FallingElement />
 
       {/* Top Brands & Agencies */}
-      <section className="space-y-10 py-20">
-        <div className="mx-auto max-w-lg">
-          <h1 className="text-center text-2xl font-medium">
-            Top Brands & Agencies
-          </h1>
-        </div>
-
-        <div className="relative overflow-hidden before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-1/6 before:bg-gradient-to-r before:from-background before:to-foreground/0 before:content-[''] after:absolute after:right-0 after:top-0 after:z-10 after:h-full after:w-1/6 after:bg-gradient-to-r after:from-foreground/0 after:to-background after:content-['']">
-          <Marquee pauseOnClick autoFill>
-            {companies.map((company, i) => (
-              <div
-                key={i}
-                className="mx-20 flex size-56 select-none items-center justify-center overflow-hidden rounded-full bg-muted text-center text-2xl font-medium"
-              >
-                {company}
-              </div>
-            ))}
-          </Marquee>
-        </div>
-      </section>
+      <BrandsAndAgencies />
 
       {/* Contact */}
       <section>
