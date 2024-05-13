8ab750c947b5492f50ce3ba511010e30c6f361b3
Rifat Khan <rifat6633@gmail.com>
2024-05-13 22:45:54 +0600

    create: seperate component for features section

--- app/(home-page-design)/home-pages/02/page.tsx
+++ app/(home-page-design)/home-pages/02/page.tsx
@@ -1,18 +1,9 @@
 import Balancer from "react-wrap-balancer";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "./_uis/carousel";
-import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -25,24 +16,7 @@ import { Button } from "@/components/ui/button";
 import Marquee from "react-fast-marquee";
 import Banner from "./_sections/banner";
 import Products from "./_sections/products";
-
-const features = [
-  {
-    title: "Web Site",
-  },
-  {
-    title: "Plugins",
-  },
-  {
-    title: "Analytics",
-  },
-  {
-    title: "API Service",
-  },
-  {
-    title: "Mobile App",
-  },
-];
+import Features from "./_sections/features";
 
 const whyChooseUs = [
   {
@@ -177,50 +151,7 @@ export default function Page() {
       <Products />
 
       {/* Features */}
-      <section className="mx-auto max-w-6xl space-y-24 pb-20">
-        <div className="mx-auto max-w-lg">
-          <h1 className="text-center text-4xl font-medium">Our Key Features</h1>
-          <p className="text-center text-muted-foreground">
-            We bridge the gap between advertisers and publishers with websites
-            or apps that can display those ads.
-          </p>
-        </div>
-
-        <div>
-          <Carousel opts={{ loop: true }} className="space-y-6">
-            <div className="flex items-center justify-end gap-3">
-              <CarouselPrevious />
-              <CarouselNext />
-            </div>
-
-            <CarouselContent>
-              {features.map((feature, i) => (
-                <CarouselItem key={i} className="basis-1/3">
-                  <Card>
-                    <CardHeader>
-                      <CardTitle className="text-center">
-                        {feature.title}
-                      </CardTitle>
-                    </CardHeader>
-
-                    <CardContent>
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                      Quis sapiente saepe nulla amet sunt odio doloremque,
-                      distinctio optio tempore. Voluptatum recusandae
-                      repudiandae quia iure voluptas, reiciendis modi quidem nam
-                      itaque.
-                    </CardContent>
-
-                    <CardFooter className="justify-center border-t p-6">
-                      <Link href="#">Learn More</Link>
-                    </CardFooter>
-                  </Card>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-          </Carousel>
-        </div>
-      </section>
+      <Features />
 
       <Separator className="bg-primary" />
 
