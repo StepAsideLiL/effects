57dcfa1f5a24c765277391f3ffdcc980d877e039
Rifat Khan <rifat6633@gmail.com>
2024-05-16 11:41:02 +0600

    add: padding

--- app/(home-page-design)/home-pages/02/layout.tsx
+++ app/(home-page-design)/home-pages/02/layout.tsx
@@ -72,7 +72,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       {children}
 
       {/* Footer */}
-      <footer className="bg-slate-800 py-10">
+      <footer className="bg-slate-800 py-20">
         <div className="mx-auto max-w-4xl space-y-10">
           <div className="flex justify-between gap-10 text-muted-foreground">
             <div className="space-y-8">
